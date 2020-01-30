11e310765d533d60b21f68ff5de95b04381f223e
Will Lewis <will@wflewis.com>
2020-01-29 19:55:33 -0500

    Update saturn.rb to support listening to files

--- saturn.rb
+++ saturn.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+
+require "listen"
+require "optparse"
+require "fileutils"
+require "open3"
+
 class Saturn
   def self.render(input) # string -> string
     text_lines = []
@@ -6,6 +12,7 @@ class Saturn
     blocks = []
     state = :text # text | code | hiddencode
     pipes = []
+    original_stdout = $stdout
 
     (input + "% hidden ruby\n% end\n").lines.each do |line|
       case state
@@ -50,6 +57,8 @@ class Saturn
         .map(&:first)
         .map(&:read)
 
+    $stdout = original_stdout
+
     blocks
       .filter { |x| x[:type] == :text }
       .map { |x| x[:body] }
@@ -59,9 +68,54 @@ class Saturn
           .map { |x| x[:hidden] ? "" : "```\n#{x[:body]}```" }
           .zip(renderings)
       )
+      .join("\n")
   end
 end
 
 if __FILE__ == $0
+  options = {}
+  op = OptionParser.new do |opts|
+    opts.banner = "Usage: saturn.rb [options]"
+
+    opts.on("-v", "--version", "Run verbosely") do
+      puts("saturn.rb v0")
+      exit(0)
+    end
+
+    opts.on("-l", "--listen", "Listen for changes") do
+      puts("listening for changes in directory ./")
+      FileUtils.mkdir_p("./build")
+
+      watcher = Listen.to('.') do |modified, added, removed|
+        filename = (modified || added).first
+        if filename
+          if File.extname(filename) == ".saturn"
+            output_filename = "./build/#{File.basename(filename)}.html"
+            File.open(output_filename, "w") do |f|
+              input = File.read(filename)
+              markdown = Saturn.render(input)
+              input_buffer, out_buffer = IO.pipe
+              command = "pandoc -f gfm -t html --metadata title=titleName --template=saturn"
+              stdout, stderr, status = Open3.capture3(command, :stdin_data => markdown)
+
+              if status != 0
+                $stderr.puts("'#{filename}' -> '#{output_filename}' failed")
+                $stderr.puts(stderr)
+              else
+                f.write(stdout)
+                $stderr.puts("'#{filename}' -> '#{output_filename}' successful")
+              end
+            end
+          end
+        end
+      end
+
+      watcher.start
+
+      sleep
+    end
+  end
+  op.parse!
+
   STDOUT.puts(Saturn.render(STDIN.read))
 end
